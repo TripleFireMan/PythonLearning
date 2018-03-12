e1695fe116cfe6df8756d2c95ac009873f2710d6
cheng.yan <cheng.yan@alibaba-inc.com>
2018-03-12 17:40:12 +0800

    课后题完成

--- hello.py
+++ hello.py
@@ -1,1 +1,48 @@
-python print aa
+# coding=utf-8
+
+# 字符串拼接	
+string1 = '中华'
+string2 = '人民共和国'
+string3 = '万岁'
+string4 = string1 + string2 + string3
+print string4
+
+# 字符串复制
+stringHello = '你好'
+stringTwoMutiple = stringHello * 2
+print stringTwoMutiple
+
+# 字符串长度求值，汉字的字符长度为3
+stringHelloLength = len(stringHello)
+print stringHelloLength
+
+# 数字转字符串、字符串转int、字符串转float
+stringOf22 = str(22)
+intOf89 = int('89')
+floatOf100 = float('100')
+print stringOf22 
+print intOf89
+print floatOf100
+
+age = 30
+print('you will be ' + str(age + 1) + ' age in a year')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
