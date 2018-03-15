e8ccbc8041814853a6e645dfe9c45a3c5b8b5fbc
cheng.yan <cheng.yan@alibaba-inc.com>
2018-03-15 11:23:12 +0800

    third part finished

--- third.py
+++ third.py
@@ -1,12 +1,28 @@
-name = 10
+#encoding=utf-8
+
+import math
+
+def collatz(number):
+	if number % 2 == 0:
+		print(number // 2)
+		return number // 2
+	else:
+		print(3 * number + 1)
+		return 3 * number + 1
+
 
 try:
-	div = name / 0
+	print('请输入:')
+	inputNumber = int(raw_input())	
+	print('你输入了 ' + str(inputNumber))
+	value = collatz(inputNumber)
+	print('运算结果为:' + str(value))
+	while value != 1:
+		value = collatz(value)
+		print('运算结果为:' + str(value))
+	finNumber = value
 except Exception, e:
-	print e
-	print Exception
-	print('hehe')
-# else:
-# 	print('else')
-# finally:
-# 	print('finally')
+	print('你必须输入一个整数')
+
+
+	
